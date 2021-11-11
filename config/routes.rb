50024fc8bdc649ff00bec43d591e0d908845004a
Flaminia von Grolman <flaminia@vgrolman.de>
2021-11-11 15:13:25 +0000

    list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :lists, only: [:index, :show, :create, :new]
+    resources :bookmarks, only: [:create, :new, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: 'https://www.istockphoto.com/photo/teamwork-of-business-concept-group-of-businessperson-human-resources-gm1271634341-374159141?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fmovie-poster&utm_term=movie%20poster%3A%3Asearch-aggressive-affiliates-v2%3Aa',
+    rating: [0..5].sample
+  )
+end
